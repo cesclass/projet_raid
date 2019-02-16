69b01db0af74b0e3051af5b2b589f2c0a8c60642
dcaron1 <dylan.caron81@laposte.net>
2019-02-16 13:17:06 +0100

    Ajout du commentary pour la fonction compute_nstripe

--- RAID_C/headers/couche2.h
+++ RAID_C/headers/couche2.h
@@ -3,6 +3,15 @@
 
 #include "../headers/raid_defines.h"
 
+/**
+ * @brief 
+ * Calcule le nombre de stripe nécessaire pour un certains nombre de block,
+ * en fonction du nombre de disque
+ * 
+ * @param nb_block Nombre de block
+ * @param nb_disk Nombre de disque
+ * @return int Nombre de stripe nécessaire
+ */
 int compute_nstripe(int nb_block, int nb_disk);
 
 
