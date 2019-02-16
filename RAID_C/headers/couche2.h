acfccce31151dd69218090e596272bc8604e5e4b
dcaron1 <dylan.caron81@laposte.net>
2019-02-16 13:11:27 +0100

    Ajout de la fonction compute_nstripe codé + testé

--- RAID_C/headers/couche2.h
+++ RAID_C/headers/couche2.h
@@ -1,5 +1,10 @@
 #ifndef _COUCHE2_H
 #define _COUCHE2_H
 
+#include "../headers/raid_defines.h"
+
+int compute_nstripe(int nb_block, int nb_disk);
+
+
 
 #endif
--- RAID_C/sources/couche2.c
+++ RAID_C/sources/couche2.c
@@ -1,1 +1,5 @@
 #include "../headers/couche2.h"
+
+int compute_nstripe(int nb_block, int nb_disk) {
+    return nb_block / nb_disk + !(nb_block % nb_disk == 0);
+}
