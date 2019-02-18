0280c92c4f2a485224ac42e65fa72f88275a2cf3
Guillaume Broise <g.broise@laposte.net>
2019-02-18 09:05:53 +0100

    remake computenblock : reduction drastique

--- RAID_C/sources/couche1.c
+++ RAID_C/sources/couche1.c
@@ -31,41 +31,12 @@ virtual_disk_t init_disk_raid5(DIR *directory){
 }
 
 
-/**
- * @brief calcule le nombre de block necessaire pour un fichier
- * 
- * @param filename 
- * @return int 
- */
-int compute_nblock(char filename[]){
 
-  struct dirent *current;
-  struct stat info_file;
-  int nb_block = 0;
-  DIR *directory = opendir("./"); /*repertoire ouvert avant ??*/
+int compute_nblock(int size){
 
-  /* parcours l'ensemble des fichiers de directory jusqu'a filename */
-  /* si filename non trouve exit du programme */
-  do {
-    current = readdir(directory);
-    if(current == NULL){
-      fprintf(stderr,"FILE not found\n");
-      exit(ERROR_FILE);
-    }
-  } while(strcmp(current->d_name,filename));
-
-
-  if(stat(current->d_name,&info_file) == 0){
-      off_t size = info_file.st_size;
-
-      /* si size%BLOCK_SIZE == 0 nb_block = size/BLOCK_SIZE */
-      /* sinon nb_block = size/BLOCK_SIZE */
-      nb_block = ((size%BLOCK_SIZE)==0)? size/BLOCK_SIZE : size/BLOCK_SIZE + 1;
-  }
-  closedir(directory);
-
-  return nb_block;
+   return int nb_block = ((size%BLOCK_SIZE)==0)? size/BLOCK_SIZE : size/BLOCK_SIZE + 1;
 }
+
 
 
 
