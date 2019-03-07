0b0be291fd9cf7410354e081e2748deb231e9cf2
Guillaume Broise <g.broise@laposte.net>
2019-03-07 17:18:05 +0100

    ajout fonction read_block/write_block

--- RAID_C/headers/couche1.h
+++ RAID_C/headers/couche1.h
@@ -11,10 +11,43 @@
 
 #define ERROR_FILE 1
 #define ERROR_FILE_CLOSE 2
+#define ERROR_SEEK_WRITE 3
+#define ERROR_WRITE 4
+#define ERROR_SEEK_READ 5
+#define ERROR_READ 6
 
 #define SUCCES_OFF 10
 
-//int compute_nblock(char filename[]);
+#define MAX_DISK 10
 
+
+
+/**
+ * @brief close all disk and close directory
+ * 
+ * @param directory 
+ * @param r5Disk 
+ */
+void switch_off_raid(DIR *directory, virtual_disk_t *r5Disk);
+
+/**
+ * @brief calculate number of block need for a file with his size
+ * 
+ * @param size 
+ * @return int 
+ */
+int compute_nblock(int size);
+
+/**
+ * @brief 
+ * 
+ * @param pos 
+ * @param block 
+ * @param dest 
+ * @return 3 = Write error 
+ */
+int write_block(int pos, block_t block, FILE *dest);
+
+int read_block(int pos, block_t *block, FILE *src);
 
 #endif
--- RAID_C/sources/couche1.c
+++ RAID_C/sources/couche1.c
@@ -1,23 +1,44 @@
 #include "../headers/couche1.h"
 
 
+/*=========================================================*/
+void init_super_block_raid5(virtual_disk_t *r5Disk){
+
+  r5Disk->super_block.raid_type = r5Disk->raidmode;
+  r5Disk->super_block.nb_blocks_used = 0;
+  r5Disk->super_block.first_free_byte = 0;
+}
+
 virtual_disk_t init_disk_raid5(DIR *directory){
   
-  virtual_disk_t *r5Disk;
+  virtual_disk_t *r5Disk = malloc(sizeof(virtual_disk_t));
+  r5Disk->raidmode = CINQ;
   r5Disk->number_of_files = 0;
+  r5Disk->ndisk = 0;
+  r5Disk->storage = malloc(MAX_DISK*sizeof(FILE *));
 
-  struct dirent *current;
+  init_super_block_raid5(r5Disk);
+
+  struct dirent *current = NULL;
   struct stat info_file;
 
-  opendir(directory);
+  while((current = readdir(directory))!= NULL){
 
-  while(current = readdir(directory)!= NULL){
     if(stat(current->d_name,&info_file) == 0){
+      printf("File :%s\n",current->d_name);
+      r5Disk->number_of_files ++;
       if(S_ISREG(info_file.st_mode)){
        r5Disk->number_of_files ++;
+
       }
-    }    
+      else if(S_ISBLK(info_file.st_mode)){
+        r5Disk->storage[r5Disk->ndisk ++] = fopen(current->d_name, "w");
+
+      }
+    }
+
   }
+  return *r5Disk;
 }
 
 
@@ -31,11 +52,7 @@ void switch_off_raid(DIR *directory, virtual_disk_t *r5Disk){
   closedir(directory);
   exit(SUCCES_OFF);
 }
-
-
-
-
-
+/*=========================================================*/
 
 int compute_nblock(int size){
   int nb_block;
@@ -43,11 +60,62 @@ int compute_nblock(int size){
 }
 
 
+int write_block(int pos, block_t block, FILE *dest){
+
+  int block_wr;
+
+  if(fseek(dest,pos*sizeof(block_t),SEEK_SET)){
+    perror("Error write pos");
+    return ERROR_SEEK_WRITE;
+  }
+  block_wr = fwrite(&block,sizeof(block_t),1,dest);
+
+  return (block_wr * sizeof(block_t) == 1)? EXIT_SUCCESS : ERROR_WRITE;
+}
 
 
+int read_block(int pos, block_t *block, FILE * src){
+
+  if(fseek(src, pos * sizeof(block_t), SEEK_SET)){
+    perror("Error read pos");
+    return ERROR_SEEK_READ;
+  }
+
+  return (fread(block, sizeof(block_t) ,1 , src) == 1)? EXIT_SUCCESS : ERROR_READ;
+}
 
 
+/*=========================================================*/
 int main(void){
-    int nb_block = compute_nblock("test.txt");
-    printf("Nb block %d",nb_block);
+    
+    DIR* direc = opendir("../RAID/");
+    if(!direc){
+      perror("Error directory");
+      exit(-1);
+    }
+    /*virtual_disk_t r5 = init_disk_raid5(direc);
+    printf("Nbdisk : %d\nNbfile : %d\n",r5.ndisk,r5.number_of_files);
+    printf("Raid mode : %d\n",r5.raidmode);
+    
+    switch_off_raid(direc,&r5);*/
+
+
+  FILE *src = fopen("source","w+b");
+
+  if(!src){
+    perror("Error open file");
+    exit(-2);
+  }
+
+  block_t block,block_read;
+  for(int i=0; i < BLOCK_SIZE;block.data[i++]='a');
+  printf("%d\n",write_block(0,block,src));
+
+  printf("<%d,%d> %d\n", EXIT_SUCCESS, ERROR_READ, read_block(0,&block_read,src));
+
+  write_block(2,block_read,src);
+  for(int i=0; i < BLOCK_SIZE; printf("Data read %3c\n",block_read.data[i++]));
+
+  fclose(src);
+  closedir(direc);
 }
