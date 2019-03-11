598ae0098ab532ee4539a4c2f16e5dfdf9b9f5a6
Cyril ESCLASSAN <cyril.esclassan@gmail.com>
2019-03-11 10:46:28 +0100

    Setup couche3.c/.h with placeholders functions

---
+++ RAID_C/headers/couche3.h
@@ -0,0 +1,24 @@
+/**
+ * @file couche3.h
+ * @author Cyril E (cyril.esclassan@univ-tlse3.fr)
+ * 
+ * @brief   Fichier de declaration des fonctions de 
+ *          la couche 3 pour le projet RAID
+ * 
+ * @copyright Licence MIT
+ */
+
+#ifndef __COUCHE3_H__
+#define __COUCHE3_H__
+
+// Liste des fonctions
+void read_inode_table(void);
+void write_inode_table(void);
+void delete_inode(void);
+void get_unused_inode(void);
+void init_inode(void);
+void read_super_block(void);
+void write_super_block(void);
+void set_first_free_byte(void);
+
+#endif
---
+++ RAID_C/sources/couche3.c
@@ -0,0 +1,49 @@
+/**
+ * @file couche3.c
+ * @author Cyril E (cyril.esclassan@univ-tlse3.fr)
+ * 
+ * @brief   Fichier de definition des fonctions de 
+ *          la couche 3 pour le projet RAID
+ * 
+ * @copyright Licence MIT
+ */
+
+#include "../headers/couche3.h"
+
+int main(void) {
+    // testing
+    return 0;
+}
+
+void read_inode_table(void) {
+    return;
+}
+
+void write_inode_table(void) {
+    return;
+}
+
+void delete_inode(void) {
+    return;
+}
+
+void get_unused_inode(void) {
+    return;
+}
+
+void init_inode(void) {
+    return;
+}
+
+void read_super_block(void) {
+    return;
+}
+
+void write_super_block(void) {
+    return;
+}
+
+void set_first_free_byte(void) {
+    return;
+}
+
