054257423e145be6124b647122f21b1d6d3f9e8f
Dylan Caron <dcaron81.dc@gmail.com>
2019-04-09 21:44:55 +0200

    Modif.s des fonctions déjà présentes + amélioration du commentary Prochaine étape : TESTS

--- RAID_C/headers/couche2.h
+++ RAID_C/headers/couche2.h
@@ -7,8 +7,9 @@
  * @brief 
  * Calcule le nombre de stripe nécessaire pour stoquer un certains nombre de block.
  * 
- * @param nb_block Nombre de block
- * @return int Nombre de stripe nécessaire
+ * @param nb_block      Nombre de block
+ * 
+ * @return int          Nombre de stripe nécessaire
  */
 int compute_nstripe(int nb_block);
 
@@ -17,9 +18,9 @@ int compute_nstripe(int nb_block);
  * Calcule le block de parité des block composant block_tab.
  * Le block de parité est placé dans le 3ème paramètre
  * 
- * @param nb_block Nombre de block dans block_tab
- * @param block_tab Tableau de block
- * @param parity_block Block de stockage pour la paritée
+ * @param nb_block      Nombre de block dans block_tab
+ * @param block_tab     Tableau de block
+ * @param parity_block  Block de stockage pour la paritée
  */
 void compute_parity(int nb_block, block_t *block_tab, block_t *parity_block);
 
@@ -28,30 +29,31 @@ void compute_parity(int nb_block, block_t *block_tab, block_t *parity_block);
  * Calcule le numéro du disque sur lequel doit être stoqué 
  * le bloc de parité
  * 
- * @param num_bande Numéro du stripe (/!\ démmare à 1 !)
- * @return int Numéro du disque (/!\ démmare à 0 !)
+ * @param num_stripe    Numéro du stripe (/!\ démmare à 0 !)
+ * 
+ * @return int          Numéro du disque (/!\ démmare à 0 !)
  */
-int parity_index(int num_bande);
+int parity_index(int num_stripe);
 
 /**
  * @brief 
  * Écrit une bande à une position donné sur le système RAID 5
  * 
- * @param pos Position
- * @param bande Bande à écrire
+ * @param pos           N° de la ligne ou ecrire la bande
+ * @param stripe        Bande à écrire
  */
-void write_stripe(int pos, const stripe_t *bande, int parity_index);
+void write_stripe(int pos, const stripe_t *stripe, int parity_index);
 
 /**
  * @brief 
  * Ecrit sur le système RAID 5 le contenu du buffer
  * à partir de la position start_byte
  * 
- * @param n Taille du buffer en octet
- * @param buffer Buffer à écrire
- * @param start_byte Position de départ sur le RAID
- * @param nb_disk Nombre de disque du système RAID
+ * @param buf_len       Taille du buffer en octet
+ * @param buffer        Buffer à écrire
+ * @param start_byte    Position de départ sur le RAID
+ * @param nb_disk       Nombre de disque du système RAID
  */
-void write_chunk(int n, uchar *buffer, uchar start_byte, int nb_disk);
+void write_chunk(int buf_len, uchar *buffer, uchar start_byte, int nb_disk);
 
 #endif
--- RAID_C/sources/couche2.c
+++ RAID_C/sources/couche2.c
@@ -17,54 +17,55 @@ void compute_parity(int nb_block, block_t *block_tab, block_t *parity_block) {
     }
 }
 
-int parity_index(int num_bande) {
-    return (- (num_bande % r5disk.ndisk) + r5disk.ndisk) % r5disk.ndisk;
+int parity_index(int num_stripe) {
+    return (- ((num_stripe + 1) % r5disk.ndisk) + r5disk.ndisk) % r5disk.ndisk;
 }
 
-void write_stripe(int pos, const stripe_t *bande, int parity_index) {
+void write_stripe(int pos, const stripe_t *stripe, int parity_index) {
     /**
      * @TODO
      * Tester cette fonction avec write_block dès qu'elle sera codé
-     * Ne pas oublier de vérifier les arguments 
      */
-    /*
-    for (int i = 0; i < bande->nblocks; i++) {
-        if (i < parity_index)
-            write_block(pos + i, bande->stripe[i]);
-        else if (i > parity_index)
-            write_block(pos + i, bande->stripe[i - 1]);
-        else
-            write_block(pos + i, bande->stripe[bande->nblocks - 1]);
+    
+    for (int i = 0; i < stripe->nblocks; i++) {
+        if (i < parity_index)       /*  Ecriture des blocs positionnees avant le bloc de parite */
+            write_block(pos, stripe->stripe[i], r5disk.storage[i]);
+        else if (i > parity_index)  /*  Ecriture des blocs positionnees apres le bloc de parite */
+            write_block(pos, stripe->stripe[i - 1], r5disk.storage[i]);
+        else                        /*  Ecriture du bloc de paritee */
+            write_block(pos, stripe->stripe[stripe->nblocks - 1], r5disk.storage[i]);
     }
-    */
 }
 
-void write_chunk(int n, uchar *buffer, uchar start_byte, int nb_disk) {
+void write_chunk(int buf_len, uchar *buffer, uchar start_byte, int nb_disk) {
     /**
      * @TODO
      * Tester cette fonction avec write_block dès qu'elle sera codé
-     * Ne pas oublier de vérifier les arguments 
      */
-    /*
-    stripe_t bande;
-    bande.nblocks = nb_disk;
-    bande.stripe = malloc(nb_disk * sizeof(block_t));
-    int num_bande = 0;
+    
+    stripe_t stripe;
+    stripe.nblocks = nb_disk;
+    stripe.stripe = malloc(nb_disk * sizeof(block_t));
+    int num_stripe = 0;
 
-    for (int i_buff = 0; i_buff < n;) {
+    for (int i_buff = 0; i_buff < buf_len;) {
+        /*  Construction de la stripe */
         int i_block;
         for (i_block = 0; i_block < nb_disk - 1; i_block++) {
             for (int i_octet = 0; i_octet < BLOCK_SIZE; i_octet++) {
-                if (i_buff < n)
-                    bande.stripe[i_block].data[i_octet] = buffer[i_buff++];
+                if (i_buff < buf_len)
+                    stripe.stripe[i_block].data[i_octet] = buffer[i_buff++];
                 else
-                    bande.stripe[i_block].data[i_octet] = 0;
+                    stripe.stripe[i_block].data[i_octet] = 0;
             } 
         }   
 
-        compute_parity(nb_disk - 1, bande.stripe, (bande.stripe)+i_block);
-        write_stripe(start_byte, &bande, parity_index(num_bande));
-        num_bande = (num_bande + 1) % nb_disk;
-    }
-    */
+        /*  Calcul de la paritee */
+        compute_parity(nb_disk - 1, stripe.stripe, (stripe.stripe) + i_block);
+
+        /*  Ecriture de la stripe sur le disque */
+        write_stripe(start_byte, &stripe, parity_index(num_stripe));
+
+        num_stripe = (num_stripe + 1) % nb_disk;
+    } 
 }
