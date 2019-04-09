f4ddf7b14d4c2d85361c4799699a37ef0e3492c7
Dylan Caron <dcaron81.dc@gmail.com>
2019-04-09 21:46:35 +0200

    MaJ du fichier de test

--- RAID_C/test_c2.c
+++ RAID_C/test_c2.c
@@ -6,13 +6,19 @@
 
 #define NB_DISK 4
 
-int main() {
-    printf("TEST compute_nstripe... ");
-    assert(compute_nstripe(20, 4) == 7);
-    assert(compute_nstripe(18, 4) == 6);
-    printf("OK !\n");
+virtual_disk_t r5disk;
 
-    printf("TEST compute_parity... ");
+int main() {
+    r5disk.ndisk = NB_DISK;
+    r5disk.raidmode = CINQ;
+    
+
+    printf("TEST compute_nstripe : ");
+    assert(compute_nstripe(20) == 7);
+    assert(compute_nstripe(18) == 6);
+    printf("\tOK !\n");
+
+    printf("TEST compute_parity : ");
     stripe_t s;
     s.nblocks = 4;
     s.stripe = malloc(s.nblocks * sizeof(block_t));
@@ -21,7 +27,7 @@ int main() {
         s.stripe[1].data[i] = 47;
         s.stripe[2].data[i] = 53;
     }
-    compute_parity(3, s.stripe, (s.stripe)+3);
+    compute_parity(3, s.stripe, (s.stripe) + 3);
     // Vérification que compute_parity n'aie pas cassé mon stripe
     for(int i = 0; i < BLOCK_SIZE; i ++) {
         assert(s.stripe[0].data[i] == 29);
@@ -35,19 +41,19 @@ int main() {
         assert((s.stripe[2].data[i] ^ s.stripe[3].data[i] ^ s.stripe[0].data[i]) == s.stripe[1].data[i]);
         assert((s.stripe[3].data[i] ^ s.stripe[0].data[i] ^ s.stripe[1].data[i]) == s.stripe[2].data[i]);
     }
-    printf("OK !\n");
+    printf("\tOK !\n");
 
-    printf("TEST parity_index... ");
-    assert(parity_index(1, NB_DISK) == 3);
-    assert(parity_index(2, NB_DISK) == 2);
-    assert(parity_index(3, NB_DISK) == 1);
-    assert(parity_index(4, NB_DISK) == 0);
+    printf("TEST parity_index : ");
+    assert(parity_index(0) == 3);
+    assert(parity_index(1) == 2);
+    assert(parity_index(2) == 1);
+    assert(parity_index(3) == 0);
 
-    assert(parity_index(5, NB_DISK) == 3);
-    assert(parity_index(6, NB_DISK) == 2);
-    assert(parity_index(7, NB_DISK) == 1);
-    assert(parity_index(8, NB_DISK) == 0);
-    printf("OK !\n");
+    assert(parity_index(4) == 3);
+    assert(parity_index(5) == 2);
+    assert(parity_index(6) == 1);
+    assert(parity_index(7) == 0);
+    printf("\tOK !\n");
 
     return 0;
 }
