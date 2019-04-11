673199f1bad5f582d6bda6e10a84a6a4036ac6e5
Dylan Caron <dcaron81.dc@gmail.com>
2019-04-11 22:53:31 +0200

    Modification de la visibilitée des fonctions : Tout est public !

--- RAID_C/headers/couche2.h
+++ RAID_C/headers/couche2.h
@@ -16,6 +16,56 @@
 
 /**
  * @brief 
+ * Calcule le nombre de stripe nécessaire pour stoquer un certains nombre de block.
+ * 
+ * @param nb_block      Nombre de block
+ * 
+ * @return int          Nombre de stripe nécessaire 
+ */
+int compute_nstripe(int nb_block);
+
+/**
+ * @brief 
+ * Calcule le block de parité des block composant block_tab.
+ * Le block de parité est placé dans le 3ème paramètre
+ * 
+ * @param nb_block      Nombre de block dans block_tab
+ * @param block_tab     Tableau de block
+ * @param parity_block  Block de stockage pour la paritée
+ */
+void compute_parity(int nb_block, block_t *block_tab, block_t *parity_block);
+
+/**
+ * @brief 
+ * Calcule le numéro du disque sur lequel doit être stoqué 
+ * le bloc de parité
+ * 
+ * @param num_stripe    Numéro du stripe (/!\ démmare à 0 !)
+ * 
+ * @return int          Numéro du disque (/!\ démmare à 0 !)
+ */
+int parity_index(int num_stripe);
+
+/**
+ * @brief 
+ * Ecrit une bande à une position donnee sur le systeme RAID 5
+ * 
+ * @param pos           N° de la ligne ou ecrire la Stripe
+ * @param src           Stripe à écrire
+ */
+void write_stripe(int pos, const stripe_t *src);
+
+/**
+ * @brief
+ * Lit une bande à une position donnee sur le systeme RAID 5
+ * 
+ * @param pos           N° de la ligne ou lire la Stripe
+ * @param dest          Destination de la Stripe
+ */
+void read_stripe(int pos, stripe_t *dest);
+
+/**
+ * @brief 
  * Ecrit sur le système RAID 5 le contenu du buffer
  * à partir de la position pos
  * 
--- RAID_C/sources/couche2.c
+++ RAID_C/sources/couche2.c
@@ -6,31 +6,10 @@
 
 extern virtual_disk_t r5Disk;
 
-/*  FONCTIONS PRIVEES
- *  (Non incluses dans l'interface couche2.h)
- * ******************************************* */
-
-/**
- * @brief 
- * Calcule le nombre de stripe nécessaire pour stoquer un certains nombre de block.
- * 
- * @param nb_block      Nombre de block
- * 
- * @return int          Nombre de stripe nécessaire 
- */
 int compute_nstripe(int nb_block) {
     return nb_block / (r5Disk.ndisk - 1) + !(nb_block % (r5Disk.ndisk - 1) == 0);
 }
 
-/**
- * @brief 
- * Calcule le block de parité des block composant block_tab.
- * Le block de parité est placé dans le 3ème paramètre
- * 
- * @param nb_block      Nombre de block dans block_tab
- * @param block_tab     Tableau de block
- * @param parity_block  Block de stockage pour la paritée
- */
 void compute_parity(int nb_block, block_t *block_tab, block_t *parity_block) {
     for (int i = 0; i < BLOCK_SIZE; i++) {
         parity_block->data[i] = 0;
@@ -38,26 +17,10 @@ void compute_parity(int nb_block, block_t *block_tab, block_t *parity_block) {
     }
 }
 
-/**
- * @brief 
- * Calcule le numéro du disque sur lequel doit être stoqué 
- * le bloc de parité
- * 
- * @param num_stripe    Numéro du stripe (/!\ démmare à 0 !)
- * 
- * @return int          Numéro du disque (/!\ démmare à 0 !)
- */
 int parity_index(int num_stripe) {
     return (- ((num_stripe + 1) % r5Disk.ndisk) + r5Disk.ndisk) % r5Disk.ndisk;
 }
 
-/**
- * @brief 
- * Ecrit une bande à une position donnee sur le systeme RAID 5
- * 
- * @param pos           N° de la ligne ou ecrire la Stripe
- * @param src           Stripe à écrire
- */
 void write_stripe(int pos, const stripe_t *src) {
     /*  Calcul de l'indice du bloc de parite */
     int i_parity = parity_index(pos);
@@ -72,31 +35,23 @@ void write_stripe(int pos, const stripe_t *src) {
     }
 }
 
-/**
- * @brief
- * Lit une bande à une position donnee sur le systeme RAID 5
- * 
- * @param pos           N° de la ligne ou lire la Stripe
- * @param dest          Destination de la Stripe
- */
 void read_stripe(int pos, stripe_t *dest) {
     /*  Calcul de l'indice du bloc de parite */
     int i_parity = parity_index(pos);
 
     for (int i = 0; i < dest->nblocks; i ++) {
-        if (i < i_parity)           /*  Lecture des blocs positionnees avant le bloc de parite */
-            read_block(pos, dest->stripe + i, r5Disk.storage[i]);
-        else if (i > i_parity)      /*  Lecture des blocs positionnees apres le bloc de parite */
-            read_block(pos, dest->stripe + (i - 1), r5Disk.storage[i]);
-        else                        /*  Lecture du bloc de paritee */
-            read_block(pos, dest->stripe + (dest->nblocks - 1), r5Disk.storage[i]);
+        if (i < i_parity) {             /*  Lecture des blocs positionnees avant le bloc de parite */
+            if (read_block(pos, dest->stripe + i, r5Disk.storage[i]) == ERROR_READ) 
+                block_repair(pos, i, dest->stripe + i);
+        } else if (i > i_parity) {      /*  Lecture des blocs positionnees apres le bloc de parite */
+            if (read_block(pos, dest->stripe + (i - 1), r5Disk.storage[i]) == ERROR_READ)
+                block_repair(pos, i, dest->stripe + (i - 1));
+        } else {                        /*  Lecture du bloc de paritee */
+            if (read_block(pos, dest->stripe + (dest->nblocks - 1), r5Disk.storage[i]) == ERROR_READ)
+                block_repair(pos, i, dest->stripe + (dest->nblocks - 1));
+        }
     }
 }
-
-
-/*  FONCTIONS PUBLIQUES
- *  (Incluses dans l'interface couche2.h)
- * *************************************** */
 
 void write_chunk(int buf_len, const uchar *buffer, uchar pos) {    
     stripe_t str;
