a290b6763aa17d88d3b6cf688bb6170d6a8b7c54
Cyril ESCLASSAN <cyril.esclassan@gmail.com>
2019-04-12 19:28:06 +0200

    Dev et Test des fonctions relatives aux inodes.

--- RAID_C/headers/couche3.h
+++ RAID_C/headers/couche3.h
@@ -16,16 +16,52 @@
 
 #include "../headers/raid_defines.h"
 #include "../headers/couche2.h"
+#include "../headers/couche1.h"
 
-#define INODE_TABLE_TOTAL_SIZE INODE_TABLE_SIZE*INODE_SIZE*BLOCK_SIZE
-#define INODE_BYTE_SIZE INODE_SIZE*BLOCK_SIZE
+#define INODE_TABLE_TOTAL_SIZE  INODE_TABLE_SIZE*INODE_SIZE*BLOCK_SIZE
+#define INODE_BYTE_SIZE         INODE_SIZE*BLOCK_SIZE
+
+#define ERR_UNUSED_INODE        11
 
 // Liste des fonctions
+
+/**
+ * @brief 
+ * 
+ */
 void read_inode_table(void);
+
+/**
+ * @brief 
+ * 
+ */
 void write_inode_table(void);
-void delete_inode(void);
-int get_unused_inode(void);
-void init_inode(void);
+
+/**
+ * @brief 
+ * 
+ * @param indice 
+ */
+void delete_inode(int indice);
+
+/**
+ * @brief Get the unused inode object
+ * 
+ * @return uint 
+ */
+uint get_unused_inode(void);
+
+/**
+ * @brief 
+ * 
+ * @param filename 
+ * @param size 
+ * @param first_byte 
+ * @return uint 
+ */
+uint init_inode(char * filename, uint size, uint first_byte);
+
+
 void read_super_block(void);
 void write_super_block(void);
 void set_first_free_byte(void);
--- RAID_C/sources/couche3.c
+++ RAID_C/sources/couche3.c
@@ -17,7 +17,7 @@ void read_inode_table(void) {
     uchar buff[INODE_TABLE_TOTAL_SIZE];
     read_chunk(INODE_TABLE_TOTAL_SIZE, buff, INODES_START);
 
-    inode_table_t table = (inode_t *) buff;
+    inode_t *table = (inode_t *) buff;
     for (int i = 0; i < INODE_TABLE_SIZE; i ++) {
         r5Disk.inodes[i] = table[i];
     }
@@ -28,19 +28,34 @@ void write_inode_table(void) {
     write_chunk(INODE_TABLE_TOTAL_SIZE, buff, INODES_START);
 }
 
-void delete_inode(void) {
-    return;
+void delete_inode(int indice) {
+    inode_t * inode = &r5Disk.inodes[indice];
+    for(int i = 0; i < FILENAME_MAX_SIZE; inode->filename[i++] = '\0');
+    inode->first_byte = 0;
+    inode->nblock = 0;
+    inode->size = 0;
 }
 
-int get_unused_inode(void) {
-    for(int i = 0; i < INODE_TABLE_SIZE; i++) {
+uint get_unused_inode(void) {
+    for(uint i = 0; i < INODE_TABLE_SIZE; i++) {
         if (!r5Disk.inodes[i].size) return i;
     }
-    return -1;
+    return ERR_UNUSED_INODE;
 }
 
-void init_inode(void) {
-    return;
+uint init_inode(char * filename, uint size, uint first_byte) {
+    uint indice = get_unused_inode();
+    if(indice == ERR_UNUSED_INODE) {
+        return indice;
+    }
+
+    inode_t * inode = &(r5Disk.inodes[indice]);
+    sprintf(inode->filename, filename);
+    inode->size = size;
+    inode->first_byte = first_byte;
+    inode->nblock = compute_nblock(size);
+    /*DUGAT*/
+    return 0;
 }
 
 void read_super_block(void) {
