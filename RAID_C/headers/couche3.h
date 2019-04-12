4d8b16da4a5b364f7510dd23544a3f44a82027f4
Cyril ESCLASSAN <cyril.esclassan@gmail.com>
2019-04-12 16:52:53 +0200

    Definition de read/write_inode_table (sans test)

--- RAID_C/headers/couche3.h
+++ RAID_C/headers/couche3.h
@@ -2,8 +2,9 @@
  * @file couche3.h
  * @author Cyril E (cyril.esclassan@univ-tlse3.fr)
  * 
- * @brief   Fichier de declaration des fonctions de 
- *          la couche 3 pour le projet RAID
+ * @brief   
+ * Fichier de declaration des fonctions de 
+ *  la couche 3 pour le projet RAID
  * 
  * @copyright Licence MIT
  */
@@ -11,15 +12,19 @@
 #ifndef __COUCHE3_H__
 #define __COUCHE3_H__
 
-#include <stdio.h>
 #include <stdlib.h>
+
 #include "../headers/raid_defines.h"
+#include "../headers/couche2.h"
+
+#define INODE_TABLE_TOTAL_SIZE INODE_TABLE_SIZE*INODE_SIZE*BLOCK_SIZE
+#define INODE_BYTE_SIZE INODE_SIZE*BLOCK_SIZE
 
 // Liste des fonctions
 void read_inode_table(void);
 void write_inode_table(void);
 void delete_inode(void);
-void get_unused_inode(void);
+int get_unused_inode(void);
 void init_inode(void);
 void read_super_block(void);
 void write_super_block(void);
--- RAID_C/sources/couche3.c
+++ RAID_C/sources/couche3.c
@@ -2,33 +2,41 @@
  * @file couche3.c
  * @author Cyril E (cyril.esclassan@univ-tlse3.fr)
  * 
- * @brief   Fichier de definition des fonctions de 
- *          la couche 3 pour le projet RAID
+ * @brief   
+ * Fichier de definition des fonctions de 
+ *  la couche 3 pour le projet RAID
  * 
  * @copyright Licence MIT
  */
 
 #include "../headers/couche3.h"
 
-int main(void) {
-    // testing
-    return 0;
-}
+extern virtual_disk_t r5Disk;
 
 void read_inode_table(void) {
-    return;
+    uchar buff[INODE_TABLE_TOTAL_SIZE];
+    read_chunk(INODE_TABLE_TOTAL_SIZE, buff, INODES_START);
+
+    inode_table_t table = (inode_t *) buff;
+    for (int i = 0; i < INODE_TABLE_SIZE; i ++) {
+        r5Disk.inodes[i] = table[i];
+    }
 }
 
 void write_inode_table(void) {
-    return;
+    uchar * buff = (uchar*) r5Disk.inodes;
+    write_chunk(INODE_TABLE_TOTAL_SIZE, buff, INODES_START);
 }
 
 void delete_inode(void) {
     return;
 }
 
-void get_unused_inode(void) {
-    return;
+int get_unused_inode(void) {
+    for(int i = 0; i < INODE_TABLE_SIZE; i++) {
+        if (!r5Disk.inodes[i].size) return i;
+    }
+    return -1;
 }
 
 void init_inode(void) {
@@ -46,4 +54,3 @@ void write_super_block(void) {
 void set_first_free_byte(void) {
     return;
 }
-
