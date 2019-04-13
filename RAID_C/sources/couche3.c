a9339919583b22928eda5cefc7baca2df58f5bf5
Cyril ESCLASSAN <cyril.esclassan@gmail.com>
2019-04-13 19:06:29 +0200

    Modification de la recherche d'inode vide.
    L'utilisation de size ne convient pas pour des fichiers vides.
    (filename[0] == '\0') donne de meilleurs resultats

--- RAID_C/sources/couche3.c
+++ RAID_C/sources/couche3.c
@@ -35,11 +35,7 @@ void write_inode_table(void) {
 
 void delete_inode(int indice) {
     inode_t * inode = &r5Disk.inodes[indice];
-    
-    // Remise a zero du tableau contenant le nom du fichier
-    // Autre solution : inode->filename[0] = `\0`;
-    for(int i = 0; i < FILENAME_MAX_SIZE; inode->filename[i++] = '\0');
-    
+    inode->filename[0] = '\0';
     inode->first_byte = 0;
     inode->nblock = 0;
     inode->size = 0;
@@ -48,7 +44,7 @@ void delete_inode(int indice) {
 uint get_unused_inode(void) {
     for(uint i = 0; i < INODE_TABLE_SIZE; i++) {
         // si size vaut 0, l'inode est vide
-        if (!r5Disk.inodes[i].size) return i;
+        if (r5Disk.inodes[i].filename[0] == '\0') return i;
     }
     return ERR_UNUSED_INODE;
 }
