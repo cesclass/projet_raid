d6c76603c389b0085c6331cea0d8a5cabe53b93a
Guillaume Broise <g.broise@laposte.net>
2019-04-20 17:49:24 +0200

    structuration block.java

--- RAID_JAVA/src/raid/Block.java
+++ RAID_JAVA/src/raid/Block.java
@@ -5,11 +5,37 @@ public class Block{
     private byte []data = new byte[RaidDefine.BLOCK_SIZE];
 
     /* Getters and Setters */
-    public byte getData(int indice){
+    public byte getDataI(int indice){
         return data[indice];
     }
 
     public void setDataI(int indice, byte valeur){
         data[indice] = valeur;
     }
+
+    /* A completer */
+    public int computeNBlock(int taille){
+        return 0;
+    }
+
+    /* A completer */
+    public int writeBlock(){
+        return 0;
+    }
+
+    /* A completer */
+    public int readBlock(){
+        return 0;
+    }
+
+    /* A completer */
+    public void repairBlock(){
+
+    }
+
+    /* A completer */
+    public void dumpBlockHexa(){
+
+    }
+
 }
