aee58fae7a46ab8462f12255cba84d1740146e85
Dylan Caron <dcaron81.dc@gmail.com>
2019-04-20 17:27:22 +0200

    Update Stripe.java + RaidDefine.java
     ~ Modifications des types des constantes RaidDefine (char -> int)
     + Ajout de la fonction writeChunk (dev + com)
     + Ajout de la fonction readChunk (dev + com)

--- RAID_JAVA/src/raid/RaidDefine.java
+++ RAID_JAVA/src/raid/RaidDefine.java
@@ -9,12 +9,12 @@ package raid;
  */
 public class RaidDefine {
 	public final static int ERREUR = -1;
-	public final static char BLOCK_SIZE = 4;
-	public final static char FILENAME_MAX_SIZE = 32;
+	public final static int BLOCK_SIZE = 4;
+	public final static int FILENAME_MAX_SIZE = 32;
 	public final static int MAX_FILE_SIZE = 50*1024;
-	public final static char INODE_TABLE_SIZE = 10;
+	public final static int INODE_TABLE_SIZE = 10;
 	public final static int MAX_MSG = 1024;
-	public final static char SUPER_BLOCK_SIZE = 4;
-	public final static char INODES_START = SUPER_BLOCK_SIZE * BLOCK_SIZE;
-	public final static char INODE_SIZE = 11;
+	public final static int SUPER_BLOCK_SIZE = 4;
+	public final static int INODES_START = SUPER_BLOCK_SIZE * BLOCK_SIZE;
+	public final static int INODE_SIZE = 11;
 }
--- RAID_JAVA/src/raid/Stripe.java
+++ RAID_JAVA/src/raid/Stripe.java
@@ -39,26 +39,26 @@ public class Stripe {
 	public void computeParity() {
 		for (int i = 0; i < RaidDefine.BLOCK_SIZE; i++) {
 			for (int j = 0; j < (nBlocks - 1); 
-					parity.setDataI(i, parity.getDataI(i) ^ datas[j++].getDataI(i));
+					parity.setDataI(i, parity.getDataI(i) ^ datas[j++].getDataI(i)));
 		}
 	}
 	
 	/**
-	 * Calcule le numero du disque sur lequel doit être stoque 
+	 * Calcule le numero du disque sur lequel doit etre stoque 
 	 * le bloc de parite.
 	 * 
-	 * @param numStripe		Numéro du stripe (/!\ démmare à 0 !)
+	 * @param numStripe		Numero du stripe (/!\ demmare a 0 !)
 	 * 
-	 * @return int			Numéro du disque (/!\ démmare à 0 !)
+	 * @return int			Numero du disque (/!\ demmare a 0 !)
 	 */
 	public int parityIndex(int numStripe) {
 		return ((- ((numStripe + 1) % nBlocks) + nBlocks) % nBlocks);
 	}
 	
 	/**
-	 * Ecrit une bande à une position donnee sur le systeme RAID 5.
+	 * Ecrit une bande a une position donnee sur le systeme RAID 5.
 	 * 
-	 * @param pos			N° de la ligne ou ecrire la Stripe
+	 * @param pos			N* de la ligne ou ecrire la Stripe
 	 */
 	public void writeStripe(int pos) {
 		/*	Calcul de l'indice du bloc de parite */
@@ -75,9 +75,9 @@ public class Stripe {
 	}
 	
 	/**
-	 * Lit une bande à une position donnee sur le systeme RAID 5
+	 * Lit une bande a une position donnee sur le systeme RAID 5
 	 * 
-	 * @param pos           N° de la ligne ou lire la Stripe
+	 * @param pos           Nï¿½ de la ligne ou lire la Stripe
 	 */
 	public void readStripe(int pos) {
 		/*	Calcul de l'indice du bloc de parite */
@@ -85,34 +85,80 @@ public class Stripe {
 		
 		for (int i = 0; i < nBlocks; i ++) {
 			if (i < iParity) {			/*  Lecture des blocs positionnees avant le bloc de parite */
-				if (!datas[i].readBlock(pos, i))
+				if (!datas[i].readBlock(pos, i)) {
 					datas[i].blockRepair(pos, i);
+				}
 			} else if (i > iParity)	{	/*  Lecture des blocs positionnees apres le bloc de parite */
-				if (!datas[i - 1].readBlock(pos, i))
+				if (!datas[i - 1].readBlock(pos, i)) {
 					datas[i - 1].blockRepair(pos, i);
-			} else {					/*  Lecture du bloc de paritee */
-				if (!parity.readBlock(pos, i))
+				}
+			} else {					/*  Lecture du bloc de parite */
+				if (!parity.readBlock(pos, i)) {
 					parity.blockRepair(pos, i);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Ecrit sur le systeme RAID 5 le contenu du buffer
+	 * a partir de la position pos.
+	 * 
+	 * @param bufLen		Taille du buffer en octet
+	 * @param buffer		Buffer a ecrire
+	 * @param pos			Position de depart pour l'ecriture sur le RAID (en octet)
+	 * 
+	 * @return int			Premier octet libre apres l'ecriture
+	 */
+	public int writeChunk(int bufLen, byte[] buffer, int pos) {
+		pos /= (nBlocks * RaidDefine.BLOCK_SIZE);
+		
+		for (int iBuf = 0; iBuf < bufLen;) {
+			/*	Construction de la stripe */
+			int iBlock;
+			for (iBlock = 0; iBlock < nBlocks - 1; iBlock++) {
+				for (int iByte = 0; iByte < RaidDefine.BLOCK_SIZE; iByte ++) {
+					if (iBuf < bufLen) {
+						datas[iBlock].setDataI(iByte, buffer[iBuf++]);
+					} else {
+						datas[iBlock].setDataI(iByte, '\0');
+					}
+				}
+			}
+		
+			/*	Calcul de la parite */
+			this.computeParity();
+			
+			/*	Ecriture de la Stripe sur le RAID */
+			this.writeStripe(pos++);
+		}
+		
+		return (pos * nBlocks * RaidDefine.BLOCK_SIZE);
+	}
+	
+	/**
+	 * Lit sur le système RAID 5 un nombre d'octets definis par bufLen
+	 * et les stocke dans le buffer à partir de la position pos.
+	 * 
+	 * @param bufLen		Taille du buffer en octet (et nombre d'octets a lire)
+	 * @param buffer		Buffer de stockage
+	 * @param pos			Position de depart pour la lecture sur le RAID (en octet)
+	 */
+	public void readChunk(int bufLen, byte[] buffer, int pos) {
+		pos /= (nBlocks * RaidDefine.BLOCK_SIZE);
+		
+		for (int iBuf = 0; iBuf < bufLen;) {
+			/*	Lecture d'une Stripe */
+			this.readStripe(pos++);
+			
+			/*	Ecriture de la stripe lue dans le buffer */
+			for (int iBlock = 0; iBlock < nBlocks - 1; iBlock ++) {
+				for (int iByte = 0; iByte < RaidDefine.BLOCK_SIZE; iByte ++) {
+					if (iBuf < bufLen) {
+						buffer[iBuf] = datas[iBlock].getByteI(iByte);
+					}
+				}
 			}
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
