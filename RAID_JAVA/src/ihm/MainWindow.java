a5604fd9eb394e4aca376c307a9aa375f8173adf
Cyril ESCLASSAN <cyril.esclassan@gmail.com>
2019-04-22 10:01:11 +0200

    Avancement penible de l'ihm

--- RAID_JAVA/src/ihm/MainWindow.java
+++ RAID_JAVA/src/ihm/MainWindow.java
@@ -4,10 +4,33 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class MainWindow extends JFrame {
-    
-    public MainWindow(String title) {
+	private static final long serialVersionUID = 1L;
+
+	public MainWindow(String title) {
         super(title);
 
+        // Affichage
+        setSize(400, 200);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public void constructWindow() {
+        JList lsLIST = new JList<>();
+
+        JPanel panel = new JPanel();
+        setContentPane(panel);
+    }
+
+
+
+
+
+
+
+
+
+    public void listenEvent() {
         // Action a la fermeture de la fenetre
         WindowListener l = new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
@@ -17,13 +40,6 @@ public class MainWindow extends JFrame {
         };
         addWindowListener(l);
 
-        // Ajout des elements
-        JPanel panel = new JPanel();
-        setContentPane(panel);
 
-        // Affichage
-        setSize(400, 400);
-        setResizable(false);
-        setVisible(true);
     }
 }
