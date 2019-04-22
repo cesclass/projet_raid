f2d55f98eb7dbde632a5b22cfd2b4a958faee278
Cyril ESCLASSAN <cyril.esclassan@gmail.com>
2019-04-22 11:37:51 +0200

    Je ne comprend plus rien !

--- RAID_JAVA/src/raid/VirtualDisk.java
+++ RAID_JAVA/src/raid/VirtualDisk.java
@@ -4,20 +4,22 @@ import java.io.*;
 
 public class VirtualDisk{
 
-    private SuperBlock superBlock;
-    private Inonde tabInode = new Inode[RaidDefine.INODE_TABLE_SIZE];
+    private Superblock superBlock;
+    private Inode[] tabInode = new Inode[RaidDefine.INODE_TABLE_SIZE];
     private int nbDisque = 0;
     private int nbFile = 0;
+    private RaidType raidType = RaidType.CINQ;
     /* Def raid type */
     private File []storage = new File[RaidDefine.MAX_DISK];
 
     /* Constructor */
-    public VirtualDisk(){
-
+    public VirtualDisk() {
+        this.superBlock = new Superblock();
+        for (int i = 0; i < this.tabInode.length; this.tabInode[i++] = new Inode());
     }
 
     /* Getters et Setters */
-    public File getStorage(int indice){
+    public File getStorage(int indice) {
         return storage[indice];
     }
 
