1701729d8ed80f1cbe5194466e9f038f0ebcded7
Dylan Caron <dcaron81.dc@gmail.com>
2019-05-05 10:55:30 +0200

    Commentary de Stripe.java

--- RAID_JAVA/src/raid/Stripe.java
+++ RAID_JAVA/src/raid/Stripe.java
@@ -4,24 +4,25 @@ import java.io.IOException;
 
 /**
  * La classe Stripe permet de contruire la parite de la donnee, de l'ecrire et
- * de la lire sur le systeme RAID 5
+ * 	de la lire sur le systeme RAID 5
  * 
- * @author Dylan
- * 
- * @version 19.04.20
+ * @version 19.05.05
  */
 public class Stripe {
 	private static int nBlocks = RaidDefine.MAXDISK;
 	private static Block[] datas = new Block[nBlocks - 1];
 	private static Block parity = new Block();
 
+	/**
+	 * Constructeur de Stripe
+	 */
 	public Stripe() {
 		for (int i = 0; i < nBlocks - 1; datas[i++] = new Block());
 	}
 
 	/**
-	 * Calcule le nombre de stripe necessaire pour stoquer un certains nombre de
-	 * block.
+	 * Calcule le nombre de stripe necessaire pour stoquer 
+	 *  un certains nombre de block.
 	 * 
 	 * @param nBlocks 		Nombre de block
 	 * 
@@ -41,7 +42,8 @@ public class Stripe {
 	}
 
 	/**
-	 * Calcule le numero du disque sur lequel doit etre stoque le bloc de parite.
+	 * Calcule le numero du disque sur lequel doit etre stoque 
+	 *  le bloc de parite.
 	 * 
 	 * @param numStripe 	Numero du stripe (/!\ demmare a 0 !)
 	 * 
@@ -54,7 +56,9 @@ public class Stripe {
 	/**
 	 * Ecrit une bande a une position donnee sur le systeme RAID 5.
 	 * 
+	 * @param r5Disk		Systeme RAID 5
 	 * @param pos 			N* de la ligne ou ecrire la Stripe
+	 * 
 	 * @throws IOException
 	 */
 	public void writeStripe(VirtualDisk r5Disk, int pos) throws IOException {
@@ -62,7 +66,7 @@ public class Stripe {
 		int iParity = parityIndex(pos);
 		
 		for (int i = 0; i < nBlocks; i++) {
-			if (i < iParity) {			/*	Ecriture des blocs positionnees avant le bloc de parite */
+			if (i < iParity) {				/*	Ecriture des blocs positionnees avant le bloc de parite */
 				datas[i].writeBlock(r5Disk, pos, i);
 			} else if(i > iParity) {		/*	Ecriture des blocs positionnees apres le bloc de parite */
 				datas[i - 1].writeBlock(r5Disk, pos, i);
@@ -75,7 +79,9 @@ public class Stripe {
 	/**
 	 * Lit une bande a une position donnee sur le systeme RAID 5
 	 * 
+	 * @param r5Disk		Systeme RAID 5
 	 * @param pos           N* de la ligne ou lire la Stripe
+	 * 
 	 * @throws IOException
 	 */
 	public void readStripe(VirtualDisk r5Disk, int pos) throws IOException {
@@ -106,18 +112,22 @@ public class Stripe {
 	}
 	
 	/**
-	 * Ecrit sur le systeme RAID 5 le contenu du buffer a partir de la position pos.
+	 * Ecrit sur le systeme RAID 5 le contenu du buffer
+	 *  a partir de la position pos.
 	 * 
+	 * @param r5Disk		Systeme RAID 5
 	 * @param bufLen 		Taille du buffer en octet
 	 * @param buffer 		Buffer a ecrire
 	 * @param pos    		Position de depart pour l'ecriture sur le RAID (en octet)
 	 * 
 	 * @return int 			Premier octet libre apres l'ecriture
+	 * 
 	 * @throws IOException
 	 */
 	public int writeChunk(VirtualDisk r5Disk, int bufLen, byte[] buffer, int pos) throws IOException {
 		pos /= (nBlocks * RaidDefine.BLOCK_SIZE);
 		
+		/*	Creation d'une Stripe avec les donnees du buffer */
 		for (int iBuf = 0; iBuf < bufLen;) {
 			int iBlock;
 			for (iBlock = 0; iBlock < nBlocks - 1; iBlock++) {
@@ -143,12 +153,17 @@ public class Stripe {
 	}
 	
 	/**
-	 * Lit sur le syst�me RAID 5 un nombre d'octets definis par bufLen et les stocke
-	 * dans le buffer � partir de la position pos.
+	 * Lit, a partir de la position pos, sur le systeme RAID 5,
+	 *  un nombre d'octets definis par bufLen et les stocke
+	 * 	dans le buffer.
 	 * 
+	 * @param r5Disk		Systeme RAID 5
 	 * @param bufLen 		Taille du buffer en octet (et nombre d'octets a lire)
 	 * @param buffer 		Buffer de stockage
 	 * @param pos    		Position de depart pour la lecture sur le RAID (en octet)
+	 * 
+	 * @return int 			Premier octet apres la donnee lue sur le Systeme RAID 5
+	 * 
 	 * @throws IOException
 	 */
 	public int readChunk(VirtualDisk r5Disk, int bufLen, byte[] buffer, int pos) throws IOException {
@@ -171,6 +186,11 @@ public class Stripe {
 		return (pos * nBlocks * RaidDefine.BLOCK_SIZE);
 	}
 
+	/**
+	 * Retourne un String représentant l'objet Stripe.
+	 * 
+	 * @return String		String représentant l'objet Stripe
+	 */
 	public String toString() {
 		return	"Block 0 : " + datas[0] + '\n' +
 				"Block 1 : " + datas[1] + '\n' +
