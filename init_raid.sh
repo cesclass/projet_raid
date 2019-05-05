128b6e1ecdd17c5b84c6872445fd04c70f62c100
Cyril ESCLASSAN <cyril.esclassan@gmail.com>
2019-05-05 16:15:12 +0200

    Update init_raid.sh

--- init_raid.sh
+++ init_raid.sh
@@ -1,5 +1,12 @@
 rm RAID/*
+echo "rm RAID/*"
 touch RAID/vDisk0
 touch RAID/vDisk1
 touch RAID/vDisk2
 touch RAID/vDisk3
+echo "create vDisk[0:3]"
+touch RAID/jvDisk0
+touch RAID/jvDisk1
+touch RAID/jvDisk2
+touch RAID/jvDisk3
+echo "create jvDisk[0:3]"
